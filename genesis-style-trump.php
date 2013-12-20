f2819077c09bb3dd6c5dce3f54ac05548dd2f882
Carrie Dils <carriedils@gmail.com>
2013-12-20 17:39:51 -0600

    Update genesis-style-trump.php
    Clean up.

--- genesis-style-trump.php
+++ genesis-style-trump.php
@@ -2,15 +2,15 @@
 /**
  * Genesis Style Trump
  *
- * @package   Genesis_Style_Trump
- * @author    Carrie Dils
- * @license   GPL-2.0+
- * @link      http://www.carriedils.com
- * @copyright 2013 Carrie Dils
+ * @package           Genesis_Style_Trump
+ * @author            Carrie Dils
+ * @license           GPL-2.0+
+ * @link              http://www.carriedils.com
+ * @copyright         2013 Carrie Dils
  *
  * Plugin Name:       Genesis Style Trump
  * Plugin URI:        http://wordpress.org/plugins/genesis-style-trump
- * Description:       Loads Genesis child theme stylesheet after plugin styles.
+ * Description:       Loads Genesis child theme style sheet after plugin styles.
  * Version:           1.0.0
  * Author:            Carrie Dils
  * Author URI:        http://www.carriedils.com
@@ -22,19 +22,17 @@
  */
 
 // If this file is called directly, abort.
-if ( !defined( 'ABSPATH' ) ) {
-    die( __( "Sorry, you are not allowed to access this page directly.", 'genesis-style-trump' ) );
+if ( ! defined( 'ABSPATH' ) ) {
+    die( __( 'Sorry, you are not allowed to access this page directly.', 'genesis-style-trump' ) );
 }
 
 add_action( 'genesis_setup', 'genesisstyletrump_load_stylesheet' );
 /**
-*  Move Genesis child theme style sheet to a much later priority to give any plugins a chance to load first.
+* Move Genesis child theme style sheet to a much later priority to give any plugins a chance to load first.
 *
 * @since 1.0.0
 */ 
 function genesisstyletrump_load_stylesheet() {
-
 	remove_action( 'genesis_meta', 'genesis_load_stylesheet' );
 	add_action( 'wp_enqueue_scripts', 'genesis_enqueue_main_stylesheet', 999 );
-
 }
